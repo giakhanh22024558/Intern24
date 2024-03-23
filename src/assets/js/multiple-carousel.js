70c80b52f33d8dbf407cf60a4eeb2ed61df3a08f
giakhanh22024558 <127011768+giakhanh22024558@users.noreply.github.com>
2024-03-23 16:55:52 +0700

    Add sticky header and scrollbar styling*** ***Update carousel scroll behavior*** ***Adjust input field layout*** ***Update service card content

--- src/assets/js/multiple-carousel.js
+++ src/assets/js/multiple-carousel.js
@@ -1,17 +1,26 @@
 var carouselWidth = $(".carousel-inner-2")[0].scrollWidth;
 var cardWidth = $(".carousel-item-2").width();
 var scrollPosition = 0;
+console.log(carouselWidth + " " + (cardWidth*4));
 
 $(".carousel-control-next-2").on("click", function () {
-  if (scrollPosition < (carouselWidth - cardWidth * 4)) { //check if you can go any further
-    scrollPosition += cardWidth ;  //update scroll position
-    $(".carousel-inner-2").animate({ scrollLeft: scrollPosition },600); //scroll left
+   if(scrollPosition > 632.7825){
+    scrollPosition = 632.7825;
+   }
+   console.log(scrollPosition);
+});
+
+$(".carousel-control-next-2").on("click", function () {
+
+  if (scrollPosition < (carouselWidth - cardWidth * 3)) { 
+    scrollPosition += cardWidth*1.05;  
+    $(".carousel-inner-2").animate({ scrollLeft: scrollPosition },600);
   }
 });
 
 $(".carousel-control-prev-2").on("click", function () {
   if (scrollPosition > 0) {
-    scrollPosition -= cardWidth + 16;
+    scrollPosition -= cardWidth*1.05;
     $(".carousel-inner-2").animate(
       { scrollLeft: scrollPosition },
       600
